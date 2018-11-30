c5a4e2ab997b68805a36800034b7c56846d30fef
Deepanshu1809 <Deepanshu.tuteja1@gmail.com>
2018-11-30 18:04:25 +0530

    Images Added

--- FinalProject/src/finalProject/Bullet.java
+++ FinalProject/src/finalProject/Bullet.java
@@ -1,11 +1,15 @@
 package finalProject;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Bullet {
 	
 	private int x ;
 	private int y ;
+	BufferedImage i1 ;
 	
 	
 	public Bullet(int x , int y ) {
@@ -17,12 +21,19 @@ public class Bullet {
 	}
 	
 	public void tick() {
-		y-=10;
+		y-=30;
+		
+		 //y = y + 50 ;
 	}
 	
 	public void paint(Graphics g) {
+		try {
+			i1 = ImageIO.read(Bullet.class.getResource("/imgs/bullet_1.png"));
+		}catch(Exception e) {
+			e.getMessage();
+		}
 		
-		g.fillRect((int)x , (int)y , 20 , 20);
+		g.drawImage(i1,(int)x , (int)y , 20 , 30,null);
 	}
 
 }
--- FinalProject/src/finalProject/GamePlay.java
+++ FinalProject/src/finalProject/GamePlay.java
@@ -1,21 +1,27 @@
 package finalProject;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
 public class GamePlay extends JPanel implements KeyListener , ActionListener {
 
-	Timer t;
+	Timer t , g , l;
 	Random r = new Random();
 	
+	ArrayList<Rectangle> goblins;
 	
+	BufferedImage i1  , g1 , gun;
 	
 	private Controller c;
 	
@@ -23,9 +29,9 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
 	
 	int x1 , y1 , x2 , y2 , x3 , y3 , x4 , y4 , x5 , y5 , x6 , y6 ;
 	int p_x = 500 , p_y =850;
-	int b_x , b_y;
-	int delay = 400 ;
-	
+	//int b_x , b_y;
+	int delay = 1 ;
+	int del = 300;
 	
 	
 	
@@ -36,10 +42,16 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
 		addKeyListener(this);
 		setFocusable(true);
 		t = new Timer(delay,this);
+		g = new Timer(800,this);
+		l = new Timer(del,this);
 		t.start();
-		
+		g.start();
+		l.start();
 		c = new Controller(this);
 		
+		goblins=new ArrayList<Rectangle>();
+		//Random();
+
 		
 	}
 	
@@ -68,29 +80,52 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
 		y6 = r.nextInt(400);
 		
 		
+		
+		
+		goblins.add(new Rectangle(x1,-10,50,50));
+		goblins.add(new Rectangle(x2,-10,50,50));
+		goblins.add(new Rectangle(x3,-10,50,50));
+		/*goblins.add(new Rectangle(x4,-10,50,50));
+		goblins.add(new Rectangle(x5,-10,50,50));
+		goblins.add(new Rectangle(x6,-10,50,50));*/
+	
+		
 	}
 	
 	public void paint(Graphics g) {
 		
 		super.paint(g);
+		try {
+			i1 = ImageIO.read(Bullet.class.getResource("/imgs/bg.png"));
+			g1 = ImageIO.read(Bullet.class.getResource("/imgs/g1.png"));
+			gun = ImageIO.read(Bullet.class.getResource("/imgs/gun.png"));
+		}catch(Exception e) {
+			e.getMessage();
+		}
+		g.drawImage(i1,0,0,1000,1000,null);
 		
-		g.fillOval(x1, y1, 20, 20);
+		/*g.fillOval(x1, y1, 20, 20);
 		g.fillOval(x2, y2, 30, 30);
 		g.fillOval(x3, y3, 40, 40);
 		g.fillOval(x4, y4, 50, 50);			// GoBlins
 		g.fillOval(x5, y5, 60, 60);
 		g.fillOval(x6, y6, 70, 70);
+		*/
+		
+		
+		for(Rectangle r:goblins) {
+			g.drawImage(g1,r.x, r.y, r.width, r.height,null);
+		}
 		
 		
 		
 		
-		
-		g.fillRect(b_x, b_y, 20, 20);
+		//g.fillRect(b_x, b_y, 20, 20);
 		
 		c.paint(g);
 		
 		
-		g.fillRect(p_x, p_y, 100, 100);   // GUN
+		g.drawImage(gun,p_x, p_y, 100, 100,null);   // GUN
 	}
 	@Override
 	public void keyPressed(KeyEvent arg0) {
@@ -99,7 +134,7 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
 		 
 		 if(myKey == arg0.VK_ENTER) {
 			 
-			 c.addBullet(new Bullet(p_x , p_y ));
+			 c.addBullet(new Bullet(p_x+50 , p_y ));
 			 }
 		 if(myKey == arg0.VK_LEFT) {
 			 if(p_x <= 50) {
@@ -156,8 +191,27 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
-		Random();
-		b_y -= 40 ;
+		if(arg0.getSource() == g) {
+			Random();
+			repaint();
+				
+		}
+		
+		if(arg0.getSource() == l) {
+			Rectangle myithgoblin , bullet;
+			
+			for(int i=0;i<goblins.size();i++) {
+				myithgoblin=goblins.get(i);
+				myithgoblin.y+=20;
+				//if(myithgoblin.intersects(bullet)) {
+					
+				//}
+			}
+			repaint();
+			
+		}
+		
+		//b_y -= 100 ;
 		tick();
 		repaint();
 	}
